01b42132bf44a92d5f0352a111a2d6577e939bfe
Ashutosh-at-079925877420 <ashutosh.d@girdmail.com>
2024-11-29 21:33:54 +0530

    Changed Text to Ashutosh

--- lib/student_form.dart
+++ lib/student_form.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 export 'package:student_form/Apply/LeaveApply.dart';
 
 class StudentForm extends StatelessWidget {
-
   // final Function(String routeName) navigateToMainAppPage;
 
   // StudentForm({required this.navigateToMainAppPage});
@@ -15,12 +14,11 @@ class StudentForm extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            var d=
-            Navigator.pushNamed(context, "/");
+            var d = Navigator.pushNamed(context, "/");
             // Use the callback to navigate to a page in the main app
             // navigateToMainAppPage('/details');
           },
-          child: Text("Go to Main App sakjsa Page"),
+          child: Text("Go to Main App Ashutosh Page"),
         ),
       ),
     );
